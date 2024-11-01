8dc57948002beae93d45ea6064690e2aa3a41298
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-01 11:50:49 +0530

    added upload image functionality

--- text_extraction.py
+++ text_extraction.py
@@ -1,4 +1,15 @@
 import easyocr
+from imgurpython import ImgurClient
+from dotenv import load_dotenv
+
+load_dotenv('.env.local')
+
+def upload_image(path:str) -> None:
+    client = ImgurClient("cb30ede881b8f55","7b67ef91de5555e1ce8929f13cac909b8e1dfaf6")
+    # Upload the image
+    uploaded_image = client.upload_from_path(path, anon=True)
+    # Return the URL of the uploaded image
+    return uploaded_image['link']
 
 def readText(image:str) -> str:   
     reader = easyocr.Reader(['en'])
