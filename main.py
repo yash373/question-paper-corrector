d9f07c5f99d0eb95235e7ba1d7164dc6094339f6
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-01 12:27:30 +0530

    cleaned up directory

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from llm import correct_answer_sheet
 from text_extraction import upload_image
+import os
 
 if __name__ == "__main__":
-    total = int(input("Enter total marks: "))
+    directory = os.listdir("./answer_sheets")
+
+    user_answer = []
+
+    for file in directory:
+        user_answer.append(upload_image(f"./answer_sheets/{file}"))
 
     answer_key = upload_image("answer_key.jpg")
 
-    user_answer = [upload_image("answer_sheet.jpg")]
+    total = int(input("Enter total marks: "))
 
     print(correct_answer_sheet(answer_key, total, user_answer))
