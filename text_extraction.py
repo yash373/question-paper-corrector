0347ea52311d599bd674280393a53451b9198b73
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-01 12:22:29 +0530

    removed easy ocr as it is not used any more

--- text_extraction.py
+++ text_extraction.py
@@ -1,4 +1,3 @@
-import easyocr
 from imgurpython import ImgurClient
 from dotenv import load_dotenv
 
@@ -10,10 +9,3 @@ def upload_image(path:str) -> None:
     uploaded_image = client.upload_from_path(path, anon=True)
     # Return the URL of the uploaded image
     return uploaded_image['link']
-
-def readText(image:str) -> str:   
-    reader = easyocr.Reader(['en'])
-
-    result = reader.readtext(image, detail=1, paragraph=True)
-
-    return (result[0][-1])
